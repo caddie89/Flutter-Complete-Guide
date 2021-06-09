b486e40257150c60a31ffc8620e7b510a6f0413e
Caleb Addie <Caleb.Addie@e-tcc.com>
2021-06-09 10:50:52 -0400

    Incrementing list index item, changed to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@
 // "material.dart" unlocks core features needed to run the app
 import 'package:flutter/material.dart';
 
-void main() {
-  // Default function that will be run first
-  // "runApp()" is provided by "material.dart"
-  runApp(MyApp());
-}
+// void main() {
+// Default function that will be run first - automatically executed
+// "runApp()" is provided by "material.dart" - function that takes a widget
+// and runs it on the screen - needs to extended either StatelessWidget or
+// StatefulWidget
+// runApp(MyApp());
+//}
+
+void main() => runApp(MyApp());
 
 // StatelessWidget allows for the creation of custom widgets
 // "build" only takes one positional argument of class BuildContext, the argument
@@ -16,8 +20,68 @@ void main() {
 // "MaterialApp()" is provided by "material.dart", it is utilizing a constructor
 // "home" is the core widget which flutter will bring onto the screen
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  // data here can be rebuilt
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  // data here is persistent
+  // State holds the data used by the build()
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext ctx) {
-    return MaterialApp(home: Text("Hello!"));
+    List<String> questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed:
+                  answerQuestion, // passing a pointer to the function, not the function result
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () =>
+                  print('Answer 2 chosen!'), // anonymous function/method
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                // ...
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+          // the <Widget>[] tells Dart it will hold a list of Widgets
+        ),
+      ),
+    );
+    // Scaffold gives a base page design, basic design, structure, and color scheme
+    // Objects or Classes should always work standalone, everything for a widget
+    // should be located in the same class
   }
 }
